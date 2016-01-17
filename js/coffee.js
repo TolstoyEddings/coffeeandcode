1b37ac805fb1fb8faba575b2cf58c4ff40a4dec8
TolstoyEddings <cyberplasticmonkey@gmail.com>
2016-01-17 15:56:33 +0000

    Added coffee.js

---
+++ js/coffee.js
@@ -0,0 +1,37 @@
+// Built-in JS functions
+
+// alert("Welcome to my website");
+// confirm("Is this working?");
+
+// Primitive Data Types
+// "hello" - String
+// whole numbers - Integers
+// decimals - Floats
+// true/false - Boolean values
+// ["milk", "bread", "tea"] - Array (i.e. storing multiple values of the same type together)
+
+// Variables
+// Variables are containers for storing information, and their value can be changed
+// var username = "lbishop";
+// username = "mrothery";
+
+// jquery
+// $("nav a").html("hello jquery");
+// $(selector).action("value");
+
+// The $ talks to jquery
+// The selector - "nav a", selects what we want to affect
+// The action - .html, what we want to do to the selector
+// The value - ("hello jquery"), what we want to change the content to
+
+// $("body").css("background-color", "red");
+
+// Event listeners
+// $("h1").on("click", function(){
+//   $("body").css("background-color", "red");
+// });
+
+$(".trigger").on("click", function(){
+  // $(".trigger").css("transform", "rotate(90deg)");
+  $("nav").slideToggle("slow");
+});
